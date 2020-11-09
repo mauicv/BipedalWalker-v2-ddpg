Fix positive policy loss and stop critic gradients

main.py
@@ -3,6 +3,8 @@ from src.agent import Agent
 import click
 import os
 import shutil
+from time import time
+import numpy as np
 
 from src.memory import ReplayBuffer
 from src.train import Train
@@ -32,7 +34,13 @@ def cli(ctx):
 @click.option('--steps', '-s', default=200, type=int,
               help='Max number of steps per episode')
 def train(ctx, episodes, steps):
-    logger = Logging(['episode', 'rewards', 'episode_length'])
+    logger = Logging([
+        'episode',
+        'rewards',
+        'episode_length',
+        'epsiode_run_time',
+        'average_step_run_time'
+    ])
 
     env, state_space_dim, action_space_dim = setup_env()
     replay_buffer = ReplayBuffer(state_space_dim=state_space_dim,
@@ -43,21 +51,29 @@ def train(ctx, episodes, steps):
                   action_space_dim,
                   low_action=-1,
                   high_action=1,
+                  exploration_value=0.2,
+                  tau=0.05,
                   load=True)
 
     train = Train(discount_factor=0.99,
-                  actor_learning_rate=0.00001,
-                  critic_learning_rate=0.00001)
+                  actor_learning_rate=0.00005,
+                  critic_learning_rate=0.0005)
 
     for episode in range(episodes):
-        state = env.reset()
+        state = np.array(env.reset(), dtype='float32')
         episode_reward = 0
         step_count = 0
         done = False
+        episode_start_time = time()
+        step_times = []
         while not done and step_count < steps:
+            step_time_start = time()
             step_count += 1
+
+            # training code
             action = agent.get_action(state[None], with_exploration=True)[0]
             next_state, reward, done, _ = env.step(action)
+
             replay_buffer.push(state, next_state, action, reward, done)
             episode_reward += reward
             if replay_buffer.ready:
@@ -65,25 +81,38 @@ def train(ctx, episodes, steps):
                     rewards, dones = replay_buffer.sample()
                 train(agent, states, next_states, actions, rewards, dones)
                 agent.track_weights()
-
-        logger.log([episode, episode_reward, step_count])
+            step_time_end = time()
+            step_times.append(step_time_end - step_time_start)
+        episode_end_time = time()
+        epsiode_time = episode_end_time - episode_start_time
+        average_step_time = np.array(step_times).mean()
+        logger.log([
+            episode,
+            episode_reward,
+            step_count,
+            epsiode_time,
+            average_step_time,
+        ])
         agent.save_models()
 
 
 @cli.command()
 @click.pass_context
-@click.option('--steps', '-s', default=1000, type=int,
+@click.option('--steps', '-s', default=100, type=int,
               help='Max number of steps per episode')
-def play(ctx, steps):
+@click.option('--noise', '-n', is_flag=True,
+              help='With exploration')
+def play(ctx, steps, noise):
     env, state_space_dim, action_space_dim = setup_env()
     agent = Agent(state_space_dim,
                   action_space_dim,
                   low_action=-1,
                   high_action=1,
+                  exploration_value=0.2,
                   load=True)
     state = env.reset()
     for i in range(steps):
-        action = agent.get_action(state[None])[0]
+        action = agent.get_action(state[None], with_exploration=noise)[0]
         state, reward, done, _ = env \
             .step(action)
         env.render()

src/memory.py
@@ -13,22 +13,23 @@ class ReplayBuffer:
             self,
             action_space_dim,
             state_space_dim,
-            size=10000,
+            size=1000000,
             sample_size=64):
-        self.states = np.zeros((size, state_space_dim))
-        self.next_states = np.zeros((size, state_space_dim))
-        self.actions = np.zeros((size, action_space_dim))
-        self.rewards = np.zeros(size)
-        self.dones = np.zeros(size)
+        self.states = np.zeros((size, state_space_dim), dtype='float32')
+        self.next_states = np.zeros((size, state_space_dim), dtype='float32')
+        self.actions = np.zeros((size, action_space_dim), dtype='float32')
+        self.rewards = np.zeros(size, dtype='float32')
+        self.dones = np.zeros(size, dtype='float32')
         self._end_index = 0
         self.sample_size = sample_size
 
     def push(self, state, next_state, action, reward, done):
-        self.states[self._end_index % len(self.states)] = state
-        self.next_states[self._end_index % len(self.states)] = next_state
-        self.actions[self._end_index % len(self.states)] = action
-        self.rewards[self._end_index % len(self.states)] = reward
-        self.dones[self._end_index % len(self.states)] = done
+        index = self._end_index % len(self.states)
+        self.states[index] = state
+        self.next_states[index] = next_state
+        self.actions[index] = action
+        self.rewards[index] = reward
+        self.dones[index] = done
         self._end_index += 1
 
     @property

src/model.py
@@ -1,19 +1,40 @@
 import tensorflow as tf
+import numpy as np
 
 
-def build_models(state_dim, action_dim):
+def build_models(state_dim, action_dim, layer_dims=[400, 300]):
     actor = tf.keras.Sequential()
-    actor.add(tf.keras.layers.Dense(256,
-                                    activation='relu',
-                                    input_shape=(state_dim,)))
-    actor.add(tf.keras.layers.Dense(256, activation='relu'))
+    In1 = 1/np.sqrt(layer_dims[0])
+    actor.add(
+        tf.keras.layers.Dense(
+            layer_dims[0],
+            activation='relu',
+            input_shape=(state_dim,),
+            kernel_initializer=tf.random_uniform_initializer(-In1, In1),
+            bias_initializer=tf.random_uniform_initializer(-In1, In1),))
+    In2 = 1/np.sqrt(layer_dims[1])
+    actor.add(
+        tf.keras.layers.Dense(
+            layer_dims[1],
+            activation='relu',
+            kernel_initializer=tf.random_uniform_initializer(-In2, In2),
+            bias_initializer=tf.random_uniform_initializer(-In2, In2)))
     actor.add(tf.keras.layers.Dense(action_dim, activation='tanh'))
 
     critic = tf.keras.Sequential()
-    critic.add(tf.keras.layers.Dense(256,
-                                     activation='relu',
-                                     input_shape=(state_dim + action_dim,)))
-    critic.add(tf.keras.layers.Dense(256, activation='relu'))
+    critic.add(
+        tf.keras.layers.Dense(
+            layer_dims[0],
+            activation='relu',
+            input_shape=(state_dim + action_dim,),
+            kernel_initializer=tf.random_uniform_initializer(-In1, In1),
+            bias_initializer=tf.random_uniform_initializer(-In1, In1)))
+    critic.add(
+        tf.keras.layers.Dense(
+            layer_dims[1],
+            activation='relu',
+            kernel_initializer=tf.random_uniform_initializer(-In2, In2),
+            bias_initializer=tf.random_uniform_initializer(-In2, In2)))
     critic.add(tf.keras.layers.Dense(1, activation='linear'))
 
     return actor, critic

src/train.py
@@ -21,6 +21,7 @@ class Train:
         self.critic_opt = tf.keras.optimizers\
             .Adam(learning_rate=self.critic_learning_rate)
 
+    @tf.function
     def __call__(self, agent, states, next_states, actions, rewards, dones):
         """Runs ddpg algorithm on agent actor and critic networks."""
         actor_variables = agent.actor.trainable_variables
@@ -33,18 +34,18 @@ class Train:
             y = rewards[:, None] + self.discount_factor*(1 - dones) * \
                 agent.target_critic(Q_input)
             Q_input = tf.concat([states, actions], axis=1)
-            squared_error = tf.pow(y - agent.critic(Q_input), 2)
+            td_error = tf.stop_gradient(y) - agent.critic(Q_input)
+            squared_error = tf.pow(td_error, 2)
             Q_loss = tf.reduce_mean(squared_error)
         critic_grads = critic_tape.gradient(Q_loss, critic_variables)
-        self.critic_opt.apply_gradients(zip(critic_grads,
-                                        critic_variables))
+        self.critic_opt.apply_gradients(zip(critic_grads, critic_variables))
 
         # update actor
         with tf.GradientTape() as actor_tape:
             actions = agent.actor(states)
             Q_input = tf.concat([states, actions], axis=1)
-            policy_loss = tf.reduce_mean(agent.critic(Q_input))
+            policy_loss = -tf.reduce_mean(agent.critic(Q_input))
         actor_grads = actor_tape.gradient(policy_loss, actor_variables)
         self.actor_opt.apply_gradients(zip(actor_grads, actor_variables))
 
-        return Q_loss.numpy(), policy_loss.numpy()
+        # return Q_loss.numpy(), policy_loss.numpy()