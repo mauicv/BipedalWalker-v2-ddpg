Fix reward numpy array broadcasting bug

src/agent.py
@@ -57,7 +57,7 @@ class Agent:
 
     @tf.function
     def get_action(self, state, with_exploration=False):
-        action = self.actor(state)
+        action = self.actor(state)*2  # hack for pendulum action space.
         if with_exploration:
             action = action + tf.random\
                 .normal(action.shape, mean=0.0,

src/model.py
@@ -10,15 +10,18 @@ def build_models(state_dim, action_dim, layer_dims=[400, 300]):
             layer_dims[0],
             activation='relu',
             input_shape=(state_dim,),
-            kernel_initializer=tf.random_uniform_initializer(-In1, In1),
-            bias_initializer=tf.random_uniform_initializer(-In1, In1),))
+            # kernel_initializer=tf.random_uniform_initializer(-In1, In1),
+            # bias_initializer=tf.random_uniform_initializer(-In1, In1),)
+        ))
     In2 = 1/np.sqrt(layer_dims[1])
     actor.add(
         tf.keras.layers.Dense(
             layer_dims[1],
             activation='relu',
-            kernel_initializer=tf.random_uniform_initializer(-In2, In2),
-            bias_initializer=tf.random_uniform_initializer(-In2, In2)))
+            # kernel_initializer=tf.random_uniform_initializer(-In2, In2),
+            # bias_initializer=tf.random_uniform_initializer(-In2, In2))
+        ))
+    # last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
     actor.add(tf.keras.layers.Dense(action_dim, activation='tanh'))
 
     critic = tf.keras.Sequential()

src/train.py
@@ -31,11 +31,11 @@ class Train:
         with tf.GradientTape() as critic_tape:
             next_actions = agent.target_actor(next_states)
             Q_input = tf.concat([next_states, next_actions], axis=1)
-            y = rewards[:, None] + self.discount_factor*(1 - dones) * \
-                agent.target_critic(Q_input)
+            y = rewards + self.discount_factor * (1 - dones) * \
+                agent.target_critic(Q_input)[:, 0]
             Q_input = tf.concat([states, actions], axis=1)
-            td_error = tf.stop_gradient(y) - agent.critic(Q_input)
-            squared_error = tf.pow(td_error, 2)
+            td_error = tf.stop_gradient(y) - agent.critic(Q_input)[:, 0]
+            squared_error = tf.math.square(td_error)
             Q_loss = tf.reduce_mean(squared_error)
         critic_grads = critic_tape.gradient(Q_loss, critic_variables)
         self.critic_opt.apply_gradients(zip(critic_grads, critic_variables))