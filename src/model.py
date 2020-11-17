c585223ca9ce3894934ff35302975d98bdcf4ec0
mauicv <a.thornysort@gmail.com>
2020-11-17 20:36:40 +0000

    Change parameters

--- src/model.py
+++ src/model.py
@@ -12,9 +12,9 @@ def get_actor(state_dim, action_dim, layer_dims, upper_bound):
     inputs = tf.keras.layers.Input(shape=(state_dim,))
     f1 = 1. / np.sqrt(layer_dims[0])
     out = tf.keras.layers.Dense(
-        layer_dims[0], activation="relu",
-        kernel_initializer=tf.keras.initializers.RandomUniform(-f1, f1),
-        bias_initializer=tf.keras.initializers.RandomUniform(-f1, f1)
+            layer_dims[0], activation="relu",
+            kernel_initializer=tf.keras.initializers.RandomUniform(-f1, f1),
+            bias_initializer=tf.keras.initializers.RandomUniform(-f1, f1)
         )(inputs)
 
     batch1 = tf.keras.layers.BatchNormalization()(out)
@@ -22,20 +22,20 @@ def get_actor(state_dim, action_dim, layer_dims, upper_bound):
 
     f2 = 1. / np.sqrt(layer_dims[1])
     out = tf.keras.layers.Dense(
-        layer_dims[1], activation="relu",
-        kernel_initializer=tf.keras.initializers.RandomUniform(-f2, f2),
-        bias_initializer=tf.keras.initializers.RandomUniform(-f2, f2)
+            layer_dims[1], activation="relu",
+            kernel_initializer=tf.keras.initializers.RandomUniform(-f2, f2),
+            bias_initializer=tf.keras.initializers.RandomUniform(-f2, f2)
         )(layer1_activation)
 
     batch2 = tf.keras.layers.BatchNormalization()(out)
     layer2_activation = tf.nn.relu(batch2)
-    last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
 
+    last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
     outputs = tf.keras.layers.Dense(
-        action_dim,
-        activation="tanh",
-        kernel_initializer=last_init,
-        kernel_regularizer=tf.keras.regularizers.l2(0.01)
+            action_dim,
+            activation="tanh",
+            kernel_initializer=last_init,
+            kernel_regularizer=tf.keras.regularizers.l2(0.01)
         )(layer2_activation)
 
     outputs = outputs * upper_bound
@@ -47,29 +47,32 @@ def get_critic(state_dim, action_dim, layer_dims):
     state_input = tf.keras.layers.Input(shape=(state_dim))
     action_input = tf.keras.layers.Input(shape=(action_dim))
     concat = tf.keras.layers.Concatenate()([state_input, action_input])
+
     f1 = 1. / np.sqrt(layer_dims[0])
     out = tf.keras.layers.Dense(
-        layer_dims[0], activation="relu",
-        kernel_initializer=tf.keras.initializers.RandomUniform(-f1, f1),
-        bias_initializer=tf.keras.initializers.RandomUniform(-f1, f1)
+            layer_dims[0], activation="relu",
+            kernel_initializer=tf.keras.initializers.RandomUniform(-f1, f1),
+            bias_initializer=tf.keras.initializers.RandomUniform(-f1, f1)
         )(concat)
+
     batch1 = tf.keras.layers.BatchNormalization()(out)
     layer1_activation = tf.nn.relu(batch1)
 
     f2 = 1. / np.sqrt(layer_dims[1])
     out = tf.keras.layers.Dense(
-        layer_dims[1], activation="relu",
-        kernel_initializer=tf.keras.initializers.RandomUniform(-f2, f2),
-        bias_initializer=tf.keras.initializers.RandomUniform(-f2, f2)
+            layer_dims[1], activation="relu",
+            kernel_initializer=tf.keras.initializers.RandomUniform(-f2, f2),
+            bias_initializer=tf.keras.initializers.RandomUniform(-f2, f2)
         )(layer1_activation)
+
     batch2 = tf.keras.layers.BatchNormalization()(out)
     layer2_activation = tf.nn.relu(batch2)
-    last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
 
+    last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
     outputs = tf.keras.layers.Dense(
-        1, activation="linear",
-        kernel_initializer=last_init,
-        kernel_regularizer=tf.keras.regularizers.l2(0.01)
+            1, activation="linear",
+            kernel_initializer=last_init,
+            kernel_regularizer=tf.keras.regularizers.l2(0.01)
         )(layer2_activation)
 
     return tf.keras.Model([state_input, action_input], outputs)
